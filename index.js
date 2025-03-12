9678739ae9fa322ded97c06932a4bba005aa47b5
Roman <romalicevich@gmail.com>
2025-03-12 12:38:10 +0200

    allowed request from vphone-store

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
-require("dotenv").config();
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://vphone-store--vphone-store.europe-west4.hosted.app",
+];
 
 // Allow only your frontend domain
 const corsOptions = {
-  origin: "https://ly-lash-brow.onrender.com", // Replace with your frontend's URL
+  // origin: "https://ly-lash-brow.onrender.com", // Replace with your frontend's URL
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true); // Allow the request
+    } else {
+      callback(new Error("Not allowed by CORS")); // Block the request
+    }
+  },
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
 };
 
 app.use(cors(corsOptions));
+// app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -26,7 +39,7 @@ const authRouter = require("./routes/auth.router");
 app.use("/api/v1/posts", postsRouter);
 app.use("/api/v1/auth", authRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log("Server is running....");
